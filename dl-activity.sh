cb794d85f30b2b4e5012d6ef1fa8c62e4107b581
Fabio Bier <fabio@proxymail.nl>
2021-06-30 11:41:41 +0200

    Fixed directory paths.

--- dl-activity.sh
+++ dl-activity.sh
@@ -8,51 +8,54 @@ CURRTIME=$(date +%s)
 BASEIP=$(docker run --rm -it alpine wget -qO - ifconfig.me)
 VPNIP=$(docker run --rm -it --network=container:expressvpn alpine wget -qO - ifconfig.me)
 
+WORKDIR=$(pwd)
+
 if [ "${VPNIP}" != "${BASEIP}" ]; then
     echo "${CURRTIME} VPN Up" >> vpnlog.txt
 
     echo "${CURRTIME} Restarting services" >> vpnlog.txt
-    cd ../transmission
+    cd ${WORKDIR}/transmission
     docker-compose up -d
-    cd ../nzbget
+    cd ${WORKDIR}/nzbget
     docker-compose up -d
-    cd ../jackett
+    cd ${WORKDIR}/jackett
     docker-compose up -d
 else
     echo "${CURRTIME} VPN Down" >> vpnlog.txt
     
     echo "${CURRTIME} Engaging killswitch" >> vpnlog.txt
-    cd transmission
+    cd ${WORKDIR}/transmission
     docker-compose down -v
-    cd ../nzbget
+    cd ${WORKDIR}/nzbget
     docker-compose down -v
-    cd ../jackett
+    cd ${WORKDIR}/jackett
     docker-compose down -v
 
     echo "${CURRTIME} Issuing restart" >> vpnlog.txt
-    cd expressvpn
+    cd ${WORKDIR}/expressvpn
     docker-compose down -v
     docker-compose up -d
 fi
 
 if [ "$1" == "--stop" ]; then
-    cd transmission
+    cd ${WORKDIR}/transmission
     docker-compose down -v
-    cd ../nzbget
+    cd ${WORKDIR}/nzbget
     docker-compose down -v
-    cd ../jackett
+    cd ${WORKDIR}/jackett
     docker-compose down -v
 
 elif [ "$1" == "--start" ]; then
-    cd expressvpn
+    cd ${WORKDIR}/expressvpn
     docker-compose down -v
     docker-compose up -d
     sleep 15
-    cd ../transmission
+
+    cd ${WORKDIR}/transmission
     docker-compose up -d
-    cd ../nzbget
+    cd ${WORKDIR}/nzbget
     docker-compose up -d
-    cd ../jackett
+    cd ${WORKDIR}/jackett
     docker-compose up -d
 fi
 
