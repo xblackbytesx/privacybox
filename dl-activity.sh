28d2b3668a739505f5abd5db20d56d4ff98689e0
Fabio Bier <fabio.bier@dpgmm.net>
2021-06-30 11:36:52 +0200

    Added monitoring and "Killswitch" for the VPN container to prevent IP leaks.

--- dl-activity.sh
+++ dl-activity.sh
@@ -1,5 +1,40 @@
 #! /bin/bash
 
+# Example crontab user entry: 
+# * * * * * ~/privacybox-docker/dl-activity.sh >/dev/null 2>&1
+
+CURRTIME=$(date +%s)
+
+BASEIP=$(docker run --rm -it alpine wget -qO - ifconfig.me)
+VPNIP=$(docker run --rm -it --network=container:expressvpn alpine wget -qO - ifconfig.me)
+
+if [ "${VPNIP}" != "${BASEIP}" ]; then
+    echo "${CURRTIME} VPN Up" >> vpnlog.txt
+
+    echo "${CURRTIME} Restarting services" >> vpnlog.txt
+    cd ../transmission
+    docker-compose up -d
+    cd ../nzbget
+    docker-compose up -d
+    cd ../jackett
+    docker-compose up -d
+else
+    echo "${CURRTIME} VPN Down" >> vpnlog.txt
+    
+    echo "${CURRTIME} Engaging killswitch" >> vpnlog.txt
+    cd transmission
+    docker-compose down -v
+    cd ../nzbget
+    docker-compose down -v
+    cd ../jackett
+    docker-compose down -v
+
+    echo "${CURRTIME} Issuing restart" >> vpnlog.txt
+    cd expressvpn
+    docker-compose down -v
+    docker-compose up -d
+fi
+
 if [ "$1" == "--stop" ]; then
     cd transmission
     docker-compose down -v
