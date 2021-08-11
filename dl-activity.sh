698156f87f1c70ef76975485771810dee471d4eb
Fabio Bier <fabio.bier@dpgmm.net>
2021-08-12 00:28:32 +0200

    Made the docker-compose path dynamic as well.

--- dl-activity.sh
+++ dl-activity.sh
@@ -6,6 +6,7 @@
 TIMESTAMP=$(date +"%Y%m%d-%H:%M")
 
 DOCKERPATH=$(which docker)
+COMPOSEPATH=$(which docker-compose)
 
 BASEIP=$("${DOCKERPATH}" run --rm alpine /usr/bin/wget -qO - ifconfig.me)
 VPNIP=$("${DOCKERPATH}" run --rm --network=container:expressvpn alpine /usr/bin/wget -qO - ifconfig.me)
@@ -21,49 +22,49 @@ if [ "${VPNIP}" != "${BASEIP}" ]; then
 
     echo "${TIMESTAMP} Keeping services running" >> ${WORKDIR}/vpnlog.txt
     cd ${WORKDIR}/transmission
-    /usr/local/bin/docker-compose up -d
+    ${COMPOSEPATH} up -d
     cd ${WORKDIR}/nzbget
-    /usr/local/bin/docker-compose up -d
+    ${COMPOSEPATH} up -d
     cd ${WORKDIR}/prowlarr
-    /usr/local/bin/docker-compose up -d
+    ${COMPOSEPATH} up -d
 elif [ "${VPNIP}" == "${BASEIP}" ]; then
     echo "${TIMESTAMP} VPN Down" >> ${WORKDIR}/vpnlog.txt
     
     echo "${TIMESTAMP} Engaging killswitch" >> ${WORKDIR}/vpnlog.txt
     cd ${WORKDIR}/transmission
-    /usr/local/bin/docker-compose down -v
+    ${COMPOSEPATH} down -v
     cd ${WORKDIR}/nzbget
-    /usr/local/bin/docker-compose down -v
+    ${COMPOSEPATH} down -v
     cd ${WORKDIR}/prowlarr
-    /usr/local/bin/docker-compose down -v
+    ${COMPOSEPATH} down -v
 
     echo "${TIMESTAMP} Issuing VPN restart" >> ${WORKDIR}/vpnlog.txt
     cd ${WORKDIR}/expressvpn
-    /usr/local/bin/docker-compose down -v
-    /usr/local/bin/docker-compose up -d
+    ${COMPOSEPATH} down -v
+    ${COMPOSEPATH} up -d
 else
     echo "Unable to determine VPN status" >> ${WORKDIR}/vpnlog.txt
 fi
 
 if [ "$1" == "--stop" ]; then
     cd ${WORKDIR}/transmission
-    /usr/local/bin/docker-compose down -v
+    ${COMPOSEPATH} down -v
     cd ${WORKDIR}/nzbget
-    /usr/local/bin/docker-compose down -v
+    ${COMPOSEPATH} down -v
     cd ${WORKDIR}/prowlarr
-    /usr/local/bin/docker-compose down -v
+    ${COMPOSEPATH} down -v
 
 elif [ "$1" == "--start" ]; then
     cd ${WORKDIR}/expressvpn
-    /usr/local/bin/docker-compose down -v
-    /usr/local/bin/docker-compose up -d
+    ${COMPOSEPATH} down -v
+    ${COMPOSEPATH} up -d
     sleep 15
 
     cd ${WORKDIR}/transmission
-    /usr/local/bin/docker-compose up -d
+    ${COMPOSEPATH} up -d
     cd ${WORKDIR}/nzbget
-    /usr/local/bin/docker-compose up -d
+    ${COMPOSEPATH} up -d
     cd ${WORKDIR}/prowlarr
-    /usr/local/bin/docker-compose up -d
+    ${COMPOSEPATH} up -d
 fi
 
